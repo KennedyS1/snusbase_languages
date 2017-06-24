d51fe38649085e0c7639a22abcb760e4ee1be78c
KennedyS1 <raphaelhabsieger@yahoo.fr>
2017-06-24 18:47:17 +0200

    lang_fr.php
    Translation for french

---
+++ lang_fr.php
@@ -0,0 +1,53 @@
+<?php
+// language: French
+// made by:ILcUNdunX3WhZvjN5Cgu(snusbase)/@Vw4qkehQ(twitter)
+// updated on: 24.June.2017
+// WARNINGS : YOU HAVE TO CHANGE # INTO '
+// MENU
+define('_SEARCH', 'chercher');
+define('_PURCHASE', 'acheter');
+define('_SUPPORT', 'support');
+define('_ACCOUNT', 'compte');
+define('_ACCOUNT_SETTINGS', 'paramètres de votre compte');
+define('_ACCOUNT_CHANGE', 'paramètres d#affichage'); //client: website, eg font type, theme
+define('_AFFILIATE', 'parrainage');
+define('_AFFILIATE_NEW', 'voir vos gains');
+define('_AFFILIATE_KEYS', 'changer votre clé');
+// ACCOUNT
+define('_SIGN_OUT', 'se déconnectez');
+define('_WELCOME', 'Bienvenue,'); //eg Welcome, Username.
+// SUPPORT
+define('_SUPPORT', 'Vous pouvez nous contacter sur notre twitter ou en nous envoyant un mail à publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'Les heures où notre support est le plus actif sont de');
+define('_MONDAY_FRIDAY', 'du lundi à vendredi');
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'présentation des performances');
+define('_AVG_QUERY', 'temps moyen de recherche');
+define('_AVG_RESULTS', 'moyenne du nombre de résultat');
+define('_ACC_NUMBER', 'numéro de compte');
+define('_API_LOAD', 'utilisation actuelle de l#API');
+define('_SHORT_TERM', 'objectifs à court terme');
+define('_LONG_TERM', 'objectifs à long terme');
+define('_MOST_ACTIVE', 'Nous faisons tout notre possible pour améliorer les performances, et c#est pour cela que nous rendons nos statistiques accessibles à tous. Nous les réinitialisons chaque mois et quand une mise à jour de l#API sort.');
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'changer de nom d#utilisateur');
+define('_CHANGE_USERNAME_DISPLAY', 'changer le nom affiché');
+define('_CHANGE_EMAIL', 'changer l#email');
+define('_CHANGE_PASSWORD', 'changer le mot de passe');
+// PURCHASE PAGE
+define('_PURCHASE', 'acheter');
+define('_X_DAY_ACCESS', '1 jour d#accès'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'accès illimité aux recherches');
+define('_BASIC_WILDCARD', 'wildcard basique');
+define('_CUSTOM_WILDCARD', 'wildcard personnalisé');
+define('_PRIORITIZED_SUPPORT', 'priorité à l#accès au support');
+define('_PRIVATE_ENDPOINT', 'accès privé à une partie de l#API');
+define('_ALL_FEATURES', 'toutes les fonctionnalités du demo et du premium');
+define('_DAY_EMAIL_SUPPORT', 'support par mail 24/7');
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'système de parrainage');
+define('_AFFILIATE_MADE', 'argent gagné');
+define('_AFFILIATE_KEY', 'clé de parrain');
+define('_AFFILIATE_CREATE', 'créer une nouvelle clé');
+define('_AFFILIATE_WITHDRAW', 'retirer');
+?>
